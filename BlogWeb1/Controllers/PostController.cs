86fdb9c3210faa55d3c24e0cd6721db4c8aaa26b
Guilherme <cavalcante.guilherme91@gmail.com>
2018-07-18 22:58:03 -0300

    Inclusão de tela de adição de Posts

--- BlogWeb1/Controllers/PostController.cs
+++ BlogWeb1/Controllers/PostController.cs
@@ -9,24 +9,36 @@ namespace BlogWeb1.Controllers
 {
     public class PostController : Controller
     {
-        // GET: Post
-        public ActionResult Index()
-        {
-            IList<Post> cabecalho = new List<Post>()
-            {
-                new Post() {Titulo="Título", Resumo="Resumo", Categoria="Categoria"}
-            };
+        private IList<Post> lista;
 
-            IList<Post> lista = new List<Post>()
+        public PostController()
+        {
+            this.lista = new List<Post>()
             {
                 new Post() {Titulo="Harry Potter", Resumo="Pedra Filosofal", Categoria="Filme, Livro"},
                 new Post() {Titulo="Casino Royale", Resumo="007", Categoria="Filme"},
                 new Post() {Titulo="Monge e o executivo", Resumo="Romance sobre liderança", Categoria="Livro"},
                 new Post() {Titulo="New York, New York", Resumo="Sucesso de Frank Sinatra", Categoria="Música"}
             };
+        }
 
-            ViewBag.Post = cabecalho;
+        // GET: Post
+        public ActionResult Index()
+        {
             return View(lista);
+        }
+
+        public ActionResult Novo()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(Post post)
+        {
+            lista.Add(post);
+            return View("Index", lista);
         }
     }
 }
