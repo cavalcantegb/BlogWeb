cfeb4e55b728f4324fe11f69e57bd7f0c78f37ad
Douglas Padilha <douglas.padilha@techwayinfo.com.br>
2018-07-26 13:20:16 -0300

    Exercício 4.4 Instalação do Entity Framework Criação da Classe Infra/BlogContext.cs para criar o contexto. Atualização da classe PostDAO

--- BlogWeb1/DAO/PostDAO.cs
+++ BlogWeb1/DAO/PostDAO.cs
@@ -12,38 +12,19 @@ namespace BlogWeb1.DAO
     {
         public IList<Post> Lista()
         {
-            List<Post> lista = new List<Post>();
-            using (SqlConnection conexao = ConnectionFactory.CriaConexaoAberta())
+            using (BlogContext contexto = new BlogContext())
             {
-                SqlCommand comando = conexao.CreateCommand();
-                comando.CommandText = "select * from Posts";
-                SqlDataReader leitor = comando.ExecuteReader();
-                while (leitor.Read())
-                {
-                    Post post = new Post()
-                    {
-                        Id = Convert.ToInt32(leitor["id"]),
-                        Titulo = Convert.ToString(leitor["titulo"]),
-                        Resumo = Convert.ToString(leitor["resumo"]),
-                        Categoria = Convert.ToString(leitor["categoria"])
-                    };
-                    lista.Add(post);
-                }
+                var lista = contexto.Posts.ToList();
+                return lista;
             }
-
-            return lista;
         }
 
         public void Adiciona(Post post)
         {
-            using (SqlConnection cnx = ConnectionFactory.CriaConexaoAberta())
+            using (BlogContext contexto = new BlogContext())
             {
-                SqlCommand comando = cnx.CreateCommand();
-                comando.CommandText = "insert into Posts (Titulo, Resumo, Categoria) values (@titulo,@resumo,@categoria)";
-                comando.Parameters.Add(new SqlParameter("titulo", post.Titulo));
-                comando.Parameters.Add(new SqlParameter("resumo", post.Resumo));
-                comando.Parameters.Add(new SqlParameter("categoria", post.Categoria));
-                comando.ExecuteNonQuery();
+                contexto.Posts.Add(post);
+                contexto.SaveChanges();
             }
         }
     }
---
+++ BlogWeb1/Infra/BlogContext.cs
@@ -0,0 +1,18 @@
+﻿using BlogWeb1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BlogWeb1.Infra
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("name=blog")
+        {
+
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
